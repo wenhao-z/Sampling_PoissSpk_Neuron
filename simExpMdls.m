07cf4651b88b6cfa99d11723a0ed78a8c11dddfd
Wenhao Zhang <wenhaoz1@andrew.cmu.edu>
2022-01-19 16:20:13 -0600

    Update simExpMdls.m

--- simExpMdls.m
+++ simExpMdls.m
@@ -10,20 +10,23 @@ addpath(fullfile(Path_RootDir, 'simExp'));
 %%
 flagTask = 5;
 % 1. Demo of the Gibbs sampling of a hierarchical model containing s and z.
-%    This hierarchical model is used in my Cosyne 2019 study.
 % 2. For the same hierarchical model as task 3, calculate the cross entropy
 %    and classical Fisher information with model parameters.
 % 3. Demo of Poisson spiking variability (a network without recurrent connections
-%    between E neurons) samples a distribution
+%    between E neurons) samples a distribution (Figure 2 of the paper)
 % 4. Demo of a single recurrent network samples a stimulus and context
+%    (Figure 4 of the paper)
 % 5. Scan parameters of single recurrent network and compare its sampling
 %    distribution with joint posterior of stimulus and context
 % 6. The Fisher information of neurons in a recurrent network with
 %    recurrent strength
 % 7. Demo of the responses of two coupled neural networks
+%    (Figure 6 of the paper)
 % 8. Scan parameters of two coupled neural networks and compare its
-%     sampling distribution with posterior
+%    sampling distribution with posterior
+%    (Figure 7 of the paper)
 % 9. Test the sampling speed in a single recurrent net and coupled nets
+%    (Figure S6 of the paper)
 
 switch flagTask
     case 1
